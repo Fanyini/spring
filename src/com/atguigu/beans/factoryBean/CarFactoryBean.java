8ccae18a3967db3c5847dcd9ec6a30dba84f7321
Fanyin <ztx19950805@qq.com>
2017-08-30 22:57:23 +0800

    spring 通过FactoryBean 配置bean

---
+++ src/com/atguigu/beans/factoryBean/CarFactoryBean.java
@@ -0,0 +1,31 @@
+package com.atguigu.beans.factoryBean;
+
+import org.springframework.beans.factory.FactoryBean;
+/**
+ * 自定义的 FactoryBean 需要实现FactoryBean 接口
+ */
+public class CarFactoryBean implements FactoryBean<Car> {
+
+	private String brand;
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	// 返回bean对象
+	public Car getObject() throws Exception {
+		return new Car(brand, 500000);
+	}
+
+	// 返回的bean的类型
+	public Class<?> getObjectType() {
+		return Car.class;
+	}
+
+	// 是否为单实例
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+}
---
+++ src/com/atguigu/beans/factoryBean/Main.java
@@ -0,0 +1,15 @@
+package com.atguigu.beans.factoryBean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans-factoryBean.xml");
+		
+		Car car = (Car) context.getBean("carFactoryBean");
+		System.out.println(car);
+	}
+
+}
